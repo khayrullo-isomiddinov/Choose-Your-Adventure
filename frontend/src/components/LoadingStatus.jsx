65290ef61c091d812b41c9199424b8b5fd472a9d
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2025-09-15 20:17:22 +0200

    first commit

---
+++ frontend/src/components/LoadingStatus.jsx
@@ -0,0 +1,15 @@
+function LoadingStatus({theme}) {
+    return <div className="loading-container">
+        <h2>Generating Your {theme} Story</h2>
+        <div className="loading-animation">
+            <div className="spinner">
+
+            </div>
+        </div>
+        <p className="loading-info">
+            Please wait while story is being generated...
+        </p>
+    </div>
+}
+
+export default LoadingStatus;
---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      }),
+    },
+  }
+})
